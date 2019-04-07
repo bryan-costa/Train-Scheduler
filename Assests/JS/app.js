5bcca973e4f6c524a70d45946c79bfa637a50755
The_Costa <bryan.costa@roadrunner.com>
2019-04-06 21:40:46 -0700

    Pseudo Code

---
+++ Assests/JS/app.js
@@ -0,0 +1,6 @@
+// I ran out of time to code this project out...
+// initialize Firestore
+// document.querySelector() for the inputs in the form
+// log the minutes away and the starting time in firestore
+// create a function to pull the minutes and start time and calculates the minutes away
+// display each train that is submitted
